3c7764ebbe2452216a82058cad1989f5b5837923
nifanle7 <nifanle7@163.com>
2015-12-07 23:51:31 +0800

    表格行高属性及表格执行顺序说明

--- UITableView/03-单组表格/03-单组表格/ViewController.m
+++ UITableView/03-单组表格/03-单组表格/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "Hero.h"
 
-@interface ViewController () <UITableViewDataSource>
+@interface ViewController () <UITableViewDataSource,UITableViewDelegate>
 
 @property (nonatomic, strong)UITableView *tableView;
 @property (nonatomic, strong)NSArray *LOLHeroes;
@@ -36,6 +36,8 @@
         //指定数据源，相当于连线
         _tableView.dataSource = self;
         
+        _tableView.delegate = self;
+        
         [self.view addSubview:_tableView];
     }
     return _tableView;
@@ -45,6 +47,9 @@
     [super viewDidLoad];
     
     [self tableView];
+    
+//    设置行高
+//    self.tableView.rowHeight = 60;
 
 }
 
@@ -56,19 +61,51 @@
     return self.LOLHeroes.count;
 }
 
-//每个单元格的内容
+/**
+ UITableViewCellStyleDefault    默认，标题+可选图像
+ UITableViewCellStyleValue1     标题+明细+图像
+ UITableViewCellStyleValue2     不显示图像，标题+明细
+ UITableViewCellStyleSubtitle   标题+明细+图像，运用广泛
+ */
+
+//每个单元格的明细信息
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:nil];
     
     //取出英雄对象
     Hero *LOLH = self.LOLHeroes[indexPath.row];
     
+    //标题
     cell.textLabel.text = LOLH.name;
+    
+    //明细信息
+    cell.detailTextLabel.text = LOLH.intro;
+    
+    //图像
+    cell.imageView.image = [UIImage imageNamed:LOLH.icon];
     
     return cell;
     
 }
+
+# pragma mark - 计算行高
+-(CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return (indexPath.row % 2)?60:44;
+}
+
+/**
+ 表格执行顺序
+ 1> 知道总共有多少数据
+ - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+ 2> 计算每行行高
+ -(CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+ tableView继承自scrollView，要知道contentSize才能滚动，行高默认为44
+ 3> 加载明细，懒加载，只显示要显示的表格行
+
+ */
+
 
 @end
 
